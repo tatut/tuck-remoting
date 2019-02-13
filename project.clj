aac6f332cb8f064c8fbde3dc0ecda2cbce6b404d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-02-13 09:33:52 +0200

    Upgrade tuck dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tuck-remoting "20181122-alpha"
+(defproject webjure/tuck-remoting "20190213-SNAPSHOT"
   :dependencies ~(mapv (fn [[dep {ver :mvn/version}]]
                          [dep ver])
                        (read-string (slurp "deps.edn"))))
